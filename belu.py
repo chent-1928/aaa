8f24538241fa039af653472c5c2d49ee3ec9de54
chent-1928 <1928539732@qq.com>
2024-02-18 19:10:34 +0800

    a

--- belu.py
+++ belu.py
@@ -9,6 +9,8 @@ import pandas as pd
 def bleu_en(pred_seq, label_seq, k):
     pred_tokens, label_tokens = pred_seq.split(' '), label_seq.split(' ')
     len_pred, len_label = len(pred_tokens), len(label_tokens)
+    if len_label <= 1:
+        k = 1
     score = math.exp(min(0, 1 - len_label / len_pred))
     for n in range(1, k + 1):
         num_matches, label_subs = 0, collections.defaultdict(int)
